0764ca1ef366cf8c1f5193e5d85c418871fa84ea
ulaş can <cursorulas@gmail.com>
2025-09-07 17:04:20 +0300

    Refactor project structure: rename ObjectMapper.Tests to LightMapper.Tests, update project references, and change package ID to LightMapper.

--- ObjectMapper.Tests/MapperTests.cs
+++ ObjectMapper.Tests/MapperTests.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LightMapper;
-namespace ObjectMapper.Tests;
+namespace LightMapper.Tests;
 public class MapperConfigTests
 {
 	public class Person
